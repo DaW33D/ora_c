bdf50c6540710a66bee9e7bd989937330395e17c
DaW33D <zsebokdavidf@gmail.com>
2021-03-23 13:55:52 +0100

    szamok meretezessel

--- numbers.py
+++ numbers.py
@@ -17,98 +17,94 @@ class Numbers:
         self.turtle.pendown()
         self.turtle.setheading(0)
 
-    def number0(self, x, y):
+    def number0(self, x, y, size):
         self.goto(x, y, 60, 10)
 
         self.turtle.setheading(270)
         for i in range(2):
-            self.turtle.circle(-50, 180)
-            self.turtle.forward(100)
+            self.turtle.circle(-size / 2, 180)
+            self.turtle.forward(size)
 
-    def number1(self, x, y):
+    def number1(self, x, y, size):
         self.goto(x, y, 10, 40)
 
         self.turtle.setheading(90)
-        self.turtle.forward(200)
+        self.turtle.forward(size * 2)
         self.turtle.left(135)
-        self.turtle.forward(80)
+        self.turtle.forward(size * 0.8)
 
-    def number2(self, x, y):
+    def number2(self, x, y, size):
         self.goto(x, y, 15, 10)
 
         self.turtle.setheading(180)
-        self.turtle.forward(100)
+        self.turtle.forward(size)
         self.turtle.setheading(50)
-        self.turtle.forward(140)
-        self.turtle.circle(50, 220)
+        self.turtle.forward(size * 1.4)
+        self.turtle.circle(size / 2, 220)
 
-    def number3(self, x, y):
+    def number3(self, x, y, size):
         self.goto(x, y, 60, 110)
 
         self.turtle.setheading(270)
-        self.turtle.circle(50, 270)
+        self.turtle.circle(size / 2, 270)
         self.turtle.setheading(0)
-        self.turtle.circle(50, 270)
+        self.turtle.circle(size / 2, 270)
 
-
-    def number4(self, x, y):
+    def number4(self, x, y, size):
         self.goto(x, y, 30, 40)
 
         self.turtle.setheading(90)
-        self.turtle.forward(160)
+        self.turtle.forward(size * 1.6)
         self.turtle.setheading(240)
-        self.turtle.forward(130)
+        self.turtle.forward(size * 1.3)
         self.turtle.setheading(0)
-        self.turtle.forward(85)
+        self.turtle.forward(size * 0.85)
 
-    def number5(self, x, y):
+    def number5(self, x, y, size):
         self.goto(x, y, 75, 110)
 
         self.turtle.setheading(270)
-        self.turtle.circle(50, 325)
-        print(self.turtle.pos())
-        self.turtle.goto(x - 110, y + 105)
-        print(self.turtle.pos())
+        self.turtle.circle(size / 2, 325)
+        self.turtle.goto(x, y + size * 0.285)
         self.turtle.setheading(90)
-        self.turtle.forward(90)
+        self.turtle.forward(size * 0.9)
         self.turtle.setheading(0)
-        self.turtle.forward(100)
+        self.turtle.forward(size)
 
-    def number6(self, x, y):
+    def number6(self, x, y, size):
         self.goto(x, y, 60, 10)
 
         self.turtle.setheading(270)
         for i in range(2):
-            self.turtle.circle(-50, 180)
-            self.turtle.forward(20)
-        self.turtle.circle(-50, 180)
+            self.turtle.circle(-size / 2, 180)
+            self.turtle.forward(size / 5)
+        self.turtle.circle(-size / 2, 180)
         self.turtle.setheading(90)
-        self.turtle.forward(100)
-        self.turtle.circle(-50, 180)
+        self.turtle.forward(size)
+        self.turtle.circle(-size / 2, 180)
 
-    def number7(self, x, y):
+    def number7(self, x, y, size):
         self.goto(x, y, 10, 62.5)
 
         self.turtle.setheading(75)
-        self.turtle.forward(200)
+        self.turtle.forward(size * 2)
         self.turtle.setheading(180)
-        self.turtle.forward(100)
+        self.turtle.forward(size)
 
-    def number8(self, x, y):
+    def number8(self, x, y, size):
         self.goto(x, y, 120, 60)
 
-        self.turtle.setheading(180)
-        self.turtle.circle(50, 360)
-        self.turtle.circle(-40, 360)
+        self.turtle.circle(size / -2, 360)
+        self.turtle.circle(size * 0.4, 360)
 
-    def number9(self, x, y):
+    def number9(self, x, y, size):
         self.goto(x, y, 160, 110)
 
         self.turtle.setheading(90)
         for i in range(2):
-            self.turtle.circle(-50, 180)
-            self.turtle.forward(20)
-        self.turtle.circle(-50, 180)
+            self.turtle.circle(size / -2, 180)
+            self.turtle.forward(size / 5)
+        self.turtle.circle(size / -2, 180)
         self.turtle.setheading(270)
-        self.turtle.forward(100)
-        self.turtle.circle(-50, 180)
+        self.turtle.forward(size)
+        self.turtle.circle(size / -2, 180)
