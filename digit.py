f25d8ea7f06b316551fd70ce2c11018a99262e7e
Oláh Gergő <olhgeri@gmail.com>
2021-03-26 14:14:53 +0100

    kész a hármas maradék (4,5,6,7,9,0, kettőspont)xdddddddd

--- digit.py
+++ digit.py
@@ -76,6 +76,27 @@ class digit:
         self.turtle.forward(108)
         self.egyseg()
 
+    def num3(self):
+        self.turtle.penup()
+        self.turtle.right(180)
+        self.turtle.forward(450)
+        self.turtle.pendown()
+        self.egyseg()
+        self.turtle.right(90)
+        self.turtle.forward(108)
+        self.egyseg()
+        self.turtle.left(90)
+        self.egyseg()
+        self.turtle.right(360)
+        self.turtle.right(270)
+        self.egyseg()
+        self.turtle.right(180)
+        self.turtle.penup()
+        self.turtle.forward(108)
+        self.turtle.pendown()
+        self.turtle.right(270)
+        self.egyseg()
+
 
 
 
@@ -85,6 +106,7 @@ class digit:
         self.num8()
         self.num1()
         self.num2()
+        self.num3()
 
         self.screen.mainloop()
 
